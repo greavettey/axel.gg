2a8e2e84c927648d570789cf61e5774d39a0f605
Axel Greavette <vultureavatar@gmail.com>
2022-10-20 18:03:14 -0400

    added dev log stuff

--- static/hidden.js
+++ static/hidden.js
@@ -11,6 +11,7 @@ function darkMode(bypass = false) {
 }
 
 function loadMode() {
+    commit();
     console.log("\"Hit the g for dark mode\" - Axel")
     if (document.cookie.includes("darkmode=true")) {
         darkMode();
@@ -19,4 +20,27 @@ function loadMode() {
         document.cookie = "darkmode=true";
         darkMode();
     }
+}
+
+function commit() {
+    fetch("https://api.github.com/repos/greavettey/axel.gg/commits/master").then(res => {
+        res.json().then(r => {
+            if(r.message) {
+                document.querySelector("#sha-master").innerHTML += "000000"
+            }
+            let full = r.sha;
+            let cut = full.substring(0, 6);
+            document.querySelector("#sha-master").innerHTML += "<a href=\"https://github.com/greavettey/axel.gg/commit/" + full + "\">" + cut + "</a>";
+        });
+    });
+    fetch("https://api.github.com/repos/greavettey/axel.gg/commits/dev").then(res => {
+        res.json().then(r => {
+            if(r.message) {
+                document.querySelector("#sha-dev").innerHTML += "000000"
+            }
+            let full = r.sha;
+            let cut = full.substring(0, 6);
+            document.querySelector("#sha-dev").innerHTML += "<a href=\"https://github.com/greavettey/axel.gg/commit/" + full + "\">" + cut + "</a>";
+        });
+    });
 }
